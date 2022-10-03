888b4884afd895dbba5b5be8df48df18d31f5126
gcn-feloz <albertomacielkraemer@gmail.com>
2022-10-03 01:33:10 -0300

    Algumas alterações

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.print("Numero = ");
+        int numero = 5;
+        System.out.println(numero);
+
+        System.out.print("Numero é negativo:");
+        numero = -numero;
+        System.out.println(numero);
+
+        System.out.print("Numero é Positivo:");
+        numero = -numero;
+        System.out.println(numero);
+
+        System.out.print("Incrementa ");
+        numero = --numero ;
+        System.out.println(numero--);
+        System.out.println(numero);
+
+
     }
 }
